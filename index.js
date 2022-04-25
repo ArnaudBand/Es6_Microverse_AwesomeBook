3205c2bd912582412cbe8d818934823e1dff4a78
Arnaud <bandonkeyea@gmail.com>
2022-04-25 15:32:48 -0600

    Create Js file

---
+++ index.js
@@ -0,0 +1,21 @@
+import {
+  displayList, displayBookForm, displayContact, listBtn, addBtn1, contactBtn,
+} from './modules/menuClass.js';
+
+import { clockDisplay } from './modules/variables.js';
+
+import { DateTime } from './modules/luxon.js';
+
+listBtn.addEventListener('click', displayList);
+
+addBtn1.addEventListener('click', displayBookForm);
+
+contactBtn.addEventListener('click', displayContact);
+
+const timer = () => {
+  const today = DateTime.now();
+  clockDisplay.innerText = `${today.toLocaleString(DateTime.DATETIME_FULL)}`;
+};
+
+timer();
+setInterval(() => { timer(); }, 1000);
